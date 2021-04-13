efbbaf07ee6c98e9765bf213be8b95a1e329a4e6
Ben Wunderlich <ben.wunderlich@outloo.com>
2021-04-13 00:21:03 -0700

    adding merge collision
    - also now uses arrays instead of dictionary

--- main.py
+++ main.py
@@ -3,25 +3,30 @@ import pygame
 from time import sleep
 from random import randint
 import numpy as np
+from math import ceil
 
 (BLACK, WHITE) = ((0,0,0), (255,255,255))
 
-(WIDTH, HEIGHT) = (400,400)
-RADIUS = 2
+(WIDTH, HEIGHT) = (600,600)
+RADIUS = 8
 THICCNESS = RADIUS
-HALF_RADIUS = RADIUS//2
 
 #track each particle with a dictionary, key is position? value is vector
-data = set()
-data2 = np.ones((WIDTH, HEIGHT, 2))#3rd has vector for speed of particle
+#data = set()
+data = np.ones((WIDTH+1, HEIGHT+1, 2), dtype=int)#3rd has vector for speed of particle
 occupiedSquares = set()
+
+def GetRands():
+    return (randint(0,WIDTH), randint(0,HEIGHT)), (randint(-3, 3), randint(-2, 2))
 
 def Populate(numParticales=1):
     for _ in range(numParticales):
-        nextParticle = ((randint(0,WIDTH), randint(0,HEIGHT)), (randint(-3, 3), randint(-2, 2)))
-        if nextParticle[1][0] == 0 or nextParticle[1][1] == 0:
-            continue 
-        data.add(nextParticle)
+        nextPos, nextVelo = GetRands()
+        while nextVelo[0] == 0 and nextVelo[1] == 0:
+            nextPos, nextVelo = GetRands() 
+        #print(nextVelo)
+        data[nextPos[0]][nextPos[1]] = nextVelo
+        occupiedSquares.add(nextPos)
 
 def Wipe(screen):
     pygame.Surface.fill(screen, BLACK)
@@ -31,35 +36,48 @@ def BounceCheck(pos, velo):
     newPos = (pos[0]+velo[0], pos[1]+velo[1])
     newVelo = velo
 
-    if pos[0] < 0:#left wall
-        newVelo = (-velo[0], velo[1])
+    if newPos[0] < 0:#left wall
+        newVelo = (-ceil(velo[0]/1.1), velo[1])
         newPos = (-newPos[0], newPos[1])
-    elif pos[0] > WIDTH:#right wall
-        newVelo = (-velo[0], velo[1])
+
+    elif newPos[0] >= WIDTH:#right wall
+        newVelo = (-ceil(velo[0]/1.1), velo[1])
         newPos = (WIDTH - (2*(newPos[0]-WIDTH)), newPos[1])
+
     pos, velo = newPos, newVelo
 
-    if pos[1] < 0:#ceiling
-        newVelo = (velo[0], -velo[1])
+    if newPos[1] < 0:#ceiling
+        newVelo = (velo[0], -ceil(velo[1]/1.1))
         newPos = (newPos[0], -newPos[1])
-    elif pos[1] > HEIGHT:#floor
-        newVelo = (velo[0], -velo[1])
+
+    elif newPos[1] >= HEIGHT:#floor
+        newVelo = (velo[0], -ceil(velo[1]/1.1))
         newPos = (newPos[0], HEIGHT - (2*(newPos[1]-HEIGHT)))
 
+    newPos, newVelo = (int(newPos[0]), int(newPos[1])), (int(newVelo[0]), int(newVelo[1]))
     return (newPos, newVelo)
 
-def CollisionCheck(particleData):
-    pass
+def CollisionCheck(occupied):
     #if 2 in same square they become one particle with summed components
-    newData = set()
-    duplicates = set()
-    for pos, velo in particleData:
-        for x in range(pos[0]-HALF_RADIUS, pos[0]+HALF_RADIUS):
-            for y in range(pos[1]-HALF_RADIUS, pos[1]+HALF_RADIUS):
-                pass
+    newOccupied = set()
+    rejected = set()
+    for pos in occupied:
+        taken=False
+
+        for x in range(pos[0]-RADIUS, pos[0]+RADIUS):
+            for y in range(pos[1]-RADIUS, pos[1]+RADIUS):
+                if (x,y)!= pos and (x,y) in occupied and not (x,y) in rejected and not (x,y) in newOccupied:
+                    rejected.add(pos)
+                    newOccupied.add((x,y))
+                    data[x][y] =  (data[pos[0]][pos[1]][0]+data[x][y][0], 
+                    data[pos[0]][pos[1]][1]+data[x][y][1])
+                    taken = True
+        if not taken:
+            newOccupied.add(pos)
+    return newOccupied
 
 
-Populate(5)
+Populate(1000)
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 run = True
@@ -75,12 +93,17 @@ while run:
 
     Wipe(screen)
     nextFrame = set()
-    for pos, velo in data:        
-        newPos, newVelo = BounceCheck(pos, velo)
+    for pos in occupiedSquares:
+        newPos, newVelo = BounceCheck(pos, data[pos[0]][pos[1]])
 
-        nextFrame.add((newPos, newVelo))
+        nextFrame.add(newPos)
+        #print(newPos[0], newPos[1])
+        data[newPos[0]][newPos[1]] = newVelo
         pygame.draw.circle(screen, WHITE, newPos, RADIUS, THICCNESS)
         
-    data = nextFrame
+    occupiedSquares = nextFrame
+
+    occupiedSquares = CollisionCheck(occupiedSquares)
+
     pygame.display.update()
     sleep(0.01)
